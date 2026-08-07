276cbddb7449e9f6bfd3a68980f92035f26f1e30
이성연 <sungyon1736@gmail.com>
2026-08-07 15:07:56 +0900

    2026.08.07 section11 useContext 도입 및 상태/디스패치 Context 분리, 프로젝트 초기 세팅

---
+++ onebite-react/section11/src/components/TodoItem.jsx
@@ -0,0 +1,39 @@
+import "./../components/TodoItem.css";
+import { memo, useContext } from "react";
+import { TodoDispatchContext } from "../App";
+
+const TodoItem = ({id, isDone, content, date}) => {
+    const { onUpdate, onDelete } = useContext(TodoDispatchContext);
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    }
+
+    const onClickDelete = () => {
+        onDelete(id);
+    }
+
+    return (
+        <div className="TodoItem">
+            <input 
+            onChange={onChangeCheckbox}
+            readOnly 
+            checked={isDone} 
+            type="checkbox" />
+            <div className="content">{content}</div>
+            <div className="date">{new Date(date).toLocaleDateString()}</div>
+            <button onClick={onClickDelete}>삭제</button>
+        </div>
+    )
+}
+
+// 고차(HOC) 컴포넌트
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//     if(prevProps.id !== nextProps.id) return false;
+//     if(prevProps.isDone !== nextProps.isDone) return false;
+//     if(prevProps.content !== nextProps.content) return false;
+//     if(prevProps.date !== nextProps.date) return false;
+
+//     return true;    
+// });
+
+export default memo(TodoItem);
